56f5e3d6b5468ea7f226050d90529d5f91a8fbad
lazyCatSleeping <465073452@qq.com>
2019-05-14 14:34:39 +0800

    添加头组件（包括预留的图标位置和文字以及导航栏）

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div id="head" class="row">
+      <ad-head :navs="navs" :selectedNav="selectedNav"></ad-head>
+    </div>
+    <HelloWorld />
+    <div id="foot">
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import AdHead from './components/head/AdHead.vue'
+
+
+var initPage;
+
+var getNavData;
+
+initPage = function(){
+}
+
+getNavData = function(){
+  var navs = [{
+    text: "首页",
+    id: "000"
+  },{
+    text: "技术",
+    id: "010",
+    child:[{
+      text: "前端",
+      id: "011"
+    }]
+  }];
+  return navs;
+}
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    AdHead
+  },
+  data: function(){
+    initPage();
+    var navs = getNavData();
+    return {
+      navs: navs,
+      selectedNav: 0
+    }
   }
 }
 </script>
---
+++ src/components/head/AdHead.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="ad-head col-12" id="ad-head">
+		<div class="ad-head-icon col-1">
+			<div class="ad-head-icon-img"></div>
+			<div class="ad-head-icon-text">test</div>
+		</div>
+		<div class="ad-nav col-11 text-right">
+			<div class="ad-nav-elem" 
+				v-for="nav in navs" 
+				v-on:click="changeNav(nav)"
+				:class="{active: nav.id == selectedNav}"
+				:key="nav.id"
+			>
+				{{nav.text}}
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default{
+	name: "AdHead",
+	props:{
+		navs: Array,
+		selectedNav: String
+	}
+}
+</script>
+
+<style scoped>
+</style>
